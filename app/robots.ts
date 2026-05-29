c62c928a4349e0843a3f7dd66faccfee97d2b603
sadiksahinn <sadiksahin18@gmail.com>
2026-05-30 01:46:26 +0300

    Delete app directory

--- app/robots.ts
+++
@@ -1,2 +0,0 @@
-import { site } from "@/data/content";
-export default function robots(){return {rules:{userAgent:"*",allow:"/"},sitemap:`${site.url}/sitemap.xml`}}
--- app/sitemap.ts
+++
@@ -1,3 +0,0 @@
-import { site, services, regions, blogs } from "@/data/content";
-const slugify=(s:string)=>s.toLowerCase().replaceAll(' ','-');
-export default function sitemap(){const urls=["", "/blog", "/iletisim", ...services.map(s=>`/hizmetler/${s.slug}`), ...regions.map(r=>`/bolgeler/${slugify(r)}`), ...blogs.map(([slug])=>`/blog/${slug}`)];return urls.map(u=>({url:`${site.url}${u}`,lastModified:new Date(),changeFrequency:"weekly" as const,priority:u===""?1:.75}))}
